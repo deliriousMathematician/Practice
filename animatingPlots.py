1fd9f2f38a355b8379c91ba811dabd3a8837d8ec
VsauceMichael <153628198+deliriousMathematician@users.noreply.github.com>
2024-09-02 21:19:52 +0200

    sin
    refreshing memory of FuncAnimation

--- animatingPlots.py
+++ animatingPlots.py
@@ -2,8 +2,28 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as ani
 import numpy as np
 
-fig, axs = plt.subplots(1,2, figsize=(6,6))
-axs[0].plot([1,2,3,4],[2,1,4,3])
-axs[1].plot([3,4,5,6],[1,2,3,4])
-axs[0].set_title('test')
+fig, ax = plt.subplots()
+
+x = np.arange(0, 25, 0.1)
+y = np.sin(x)
+
+# line plot
+sin = ax.plot(x[0], y[0], label=r'$y=\sin{x}$')[0]
+ax.set(xlim=[0,30], ylim=[-1.5,1.5], xlabel='x', ylabel='y')
+ax.legend()
+
+def update(frame):
+    sin.set(xdata=x[:frame], ydata=y[:frame])
+    return sin
+
+## scatter plot
+# sin = ax.scatter(x[0], y[0])
+# ax.set(xlim=[0,30], ylim=[-1.5,1.5], xlabel='x', ylabel='y')
+
+# def update(frame):
+#     data = np.stack([x[frame], y[frame]]).T
+#     sin.set_offsets(data)
+#     return sin
+
+ani = ani.FuncAnimation(fig=fig, func=update, frames=250, interval=10)
 plt.show()
